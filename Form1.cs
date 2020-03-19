06e7d7c4f0c645941890149884ccd8d41f985287
goodman99111 <goodman99111@gmail.com>
2020-03-20 01:46:40 +1000

    Revert "Заготовка для регистрации и обновление проверки статуса сервера"
    This reverts commit e0b8afe7881d83ba8540e537aa34e71edfb2dc3c.

--- Form1.cs
+++ Form1.cs
@@ -9,13 +9,13 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using AUT.ServiceReference1;
+using System.Threading;
 
 namespace AUT
 {
     public partial class Form1 : Form
     {
-        //private ServerObject Server = ServerObject.getInstance();
-        
+        AuthorizationClient client = null;
 
         public Form1()
         {
@@ -74,22 +74,21 @@ namespace AUT
 
         private void btConnect_Click(object sender, EventArgs e)
         {
-            var client = new AuthorizationClient();
-            var result = client.Authorization(tbLogin.Text, tbPassword.Text);
-            if (result)
-                MessageBox.Show("Удачно");
-            else
-                MessageBox.Show("Неудачно");
-            //Check();
-            /*
-            if (tbLogin.Text != null || tbPassword.Text != null)
+            try
             {
-                Server.SendMessage($"Check {tbLogin.Text} {tbPassword.Text}");
+                var result = client.Authorization(tbLogin.Text, tbPassword.Text);
+                if (result)
+                    MessageBox.Show("Удачно");
+                else
+                    MessageBox.Show("Неудачно");
             }
-            */
+            catch(Exception ex)
+            {
+                MessageBox.Show("Не удаётся подключиться к серверу");
+            }
+
         }
 
-<<<<<<< HEAD
         
 
         private void tbLogin_TextChanged(object sender, EventArgs e)
@@ -108,12 +107,6 @@ namespace AUT
                 btConnect.Enabled = false;
             }
 
-=======
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            //Server.Connect();
-            
->>>>>>> parent of db1d6b9... Добавлена проверка на доступность сервера
         }
     }
 }
